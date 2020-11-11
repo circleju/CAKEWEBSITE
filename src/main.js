1cf1e83c0bb3c9455008da0645fdc31e337e190e
circleju <dang2hw@foxmail.com>
2020-11-11 19:12:55 +0800

    脚手架框架搭建

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+
+
+Vue.config.productionTip = false
+axios.defaults.baseURL = 'http://127.0.0.1:3000'
+Vue.prototype.axios = axios;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
